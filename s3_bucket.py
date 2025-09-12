6a6aef3f88743cb42f800e4a6be0f93fd1df98f9
Sanjay-Shr <sanjaysharmau23@gmail.com>
2025-09-12 22:21:08 +0530

    Main Commit

--- s3_bucket.py
+++ s3_bucket.py
@@ -9,5 +9,13 @@ s3 = boto3.resource("s3")
 def show_buckets(s3):
     for bucket in s3.buckets.all():
         print(bucket.name)
-        
+
+def create_bucket(s3):
+    s3.create_bucket(Bucket="Python",
+                     createBusketConfiguration={
+                         'LocationConstraint': 'us-east-2',
+                     },)
+    print("bucket created successfully")
+
+create_bucket(s3)    
 show_buckets(s3)
